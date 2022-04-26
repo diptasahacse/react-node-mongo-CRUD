e1cfbe2fcc5d240f0ed6d15bf90cc6681ebf2970
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-26 16:50:31 +0600

    add remove button with every single user card

--- src/components/MakeUser/MakeUser.js
+++ src/components/MakeUser/MakeUser.js
@@ -3,10 +3,13 @@ import React from 'react';
 const MakeUser = ({ user }) => {
     const { name, email } = user;
     return (
-        <div className='col-lg-3 col-md-4 col-1'>
-            <div className='border p-2 rounded'>
-                <h4>{name}</h4>
-                <p>{email}</p>
+        <div className='col-lg-4 col-md-2 col-1'>
+            <div className='border p-2 rounded justify-content-between d-flex align-items-center'>
+                <div>
+                    <h4>{name}</h4>
+                    <p>{email}</p>
+                </div>
+                <button className='btn btn-sm btn-danger'>Remove</button>
             </div>
 
         </div>
